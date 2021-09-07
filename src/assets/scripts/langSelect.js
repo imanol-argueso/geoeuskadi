8472ed067d9c4cd5543852ea043edbe76a9d842b
Argüeso Epelde <i-argueso@euskadi.eus>
2021-09-07 12:34:03 +0200

    v1

--- src/assets/scripts/langSelect.js
+++ src/assets/scripts/langSelect.js
@@ -2,20 +2,20 @@
 
 export var langSelect = function(){
     //Disable down language selection area with styles
-    $('.r01gLangSelector').removeAttr('id');
-	  $(".r01gLangSelector").css("display", "none");  
+    $('.r01g-lang-selector').removeAttr('id');
+	  $(".r01g-lang-selector").css("display", "none");  
   
-        var hizkuntza_ul = $(".r01gLangSelector");
-        hizkuntza_ul.clone().css("display","block").appendTo(".goib_hizkuntza");
+        var hizkuntza_ul = $(".r01g-lang-selector");
+        hizkuntza_ul.clone().css("display","block").appendTo(".goib-hizkuntza");
         hizkuntza_ul.clone().css("display","inline-block").appendTo(".language");
   
     //If it is a search
   	if (window.location.href.indexOf("r01kQry") > -1){
-  		$('.goib_hizkuntza a.r01gLangUnSelected').click(function(event) {
+  		$('.goib-hizkuntza a.r01gLangUnSelected').click(function(event) {
         var langtarget = $(this).attr('lang');
 		var hizkuntzaURL = $(this).attr("href");
 		var bilaketaurl = location.search.replace('documentLanguage.EQ.'+r01gLang,'documentLanguage.EQ.'+langtarget).replace('r01kLang='+r01gLang,'r01kLang='+langtarget);
 		$(this).attr("href", hizkuntzaURL+bilaketaurl);      
 		});
-	}  
+	}
 };
